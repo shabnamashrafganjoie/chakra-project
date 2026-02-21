4491e3a290edb65135eccebeb83129dfc555cd0b
shabnam <shabnam.ashraf.ganjoie@gmail.com>
2026-02-21 15:26:47 -0800

    create Headless UI ListBox try 2

--- src/components/ui/MultiSelectDropdown.tsx
+++ src/components/ui/MultiSelectDropdown.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Fragment, useMemo, useRef, useState, useEffect } from "react";
-import { Listbox, Transition } from "@headlessui/react";
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { useVirtualizer } from "@tanstack/react-virtual";
 import { Product } from "@/features/products/types/product.types";
@@ -115,7 +115,7 @@ export default function MultiSelectDropdown({ options, onChange }: MultiSelectDr
     <div className="w-full max-w-sm mx-auto">
       <Listbox value={selected} onChange={() => {}} as="div">
         <div className="relative mt-1">
-          <Listbox.Button
+          <ListboxButton
             className="relative w-full cursor-default rounded-lg bg-white border border-gray-300 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500 sm:text-sm"
             onClick={() => setIsOpen(!isOpen)}
           >
@@ -127,7 +127,7 @@ export default function MultiSelectDropdown({ options, onChange }: MultiSelectDr
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon className="h-5 w-5 text-gray-400" />
             </span>
-          </Listbox.Button>
+          </ListboxButton>
 
           <Transition
             as={Fragment}
@@ -136,7 +136,7 @@ export default function MultiSelectDropdown({ options, onChange }: MultiSelectDr
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options
+            <ListboxOptions
               ref={listboxRef}
               className="absolute mt-1 max-h-96 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50"
             >
@@ -211,7 +211,7 @@ export default function MultiSelectDropdown({ options, onChange }: MultiSelectDr
                       const isSelected = selected.some(s => s.id === option.id);
 
                       return (
-                        <Listbox.Option
+                        <ListboxOption
                           key={rowData.key}
                           className={({ active }) =>
                             `relative cursor-default select-none py-2 pl-10 pr-4 ${
@@ -236,13 +236,13 @@ export default function MultiSelectDropdown({ options, onChange }: MultiSelectDr
                               <CheckIcon className="h-5 w-5" />
                             </span>
                           )}
-                        </Listbox.Option>
+                        </ListboxOption>
                       );
                     }
                   })}
                 </div>
               )}
-            </Listbox.Options>
+            </ListboxOptions>
           </Transition>
         </div>
       </Listbox>
